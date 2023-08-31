21eb6fa1d02e12267d81f93fcb435e1d1351066d
ennajari <Abdellahennajari2018@gmail.com>
2023-08-31 20:50:48 +0100

    second commit

--- abdoenna.js
+++ abdoenna.js
@@ -9,10 +9,17 @@ document.addEventListener('DOMContentLoaded', function () {
             const li = document.createElement('li');
             li.innerHTML = `
                 <span>${taskText}</span>
+                <button class="completeBtn">Complete</button>
                 <button class="deleteBtn">Delete</button>
             `;
             taskList.appendChild(li);
             taskInput.value = '';
+
+            const completeBtn = li.querySelector('.completeBtn');
+            completeBtn.addEventListener('click', function () {
+                li.classList.toggle('completed');
+                completeBtn.textContent = li.classList.contains('completed') ? 'Unmark' : 'Complete';
+            });
 
             const deleteBtn = li.querySelector('.deleteBtn');
             deleteBtn.addEventListener('click', function () {
